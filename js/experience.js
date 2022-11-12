64bfff3759e3e0b6009a9fd997b9296f1e385ba4
Josefine Kristiansson <josephinekrist@hotmail.com>
2022-11-12 17:11:35 +0100

    cleaned up code

--- js/experience.js
+++ js/experience.js
@@ -1,20 +1,3 @@
-/* const xmlhttp = new XMLHttpRequest();
-xmlhttp.onload = function () {
-  const myObj = JSON.parse(this.responseText);
-  document.getElementById("demo").innerHTML = myObj.Year;
-};
-xmlhttp.open("GET", "experience.json");
-xmlhttp.send();
- */
-/* const listEl = document.querySelector(ul);
-fetch("js/data.json")
-  .then((res) => res.json())
-  .then((data) => {
-    data.forEach((year) => {
-      listEl.insertAdjacentHTML("beforeend", `<li>${post.year}</li>`);
-    });
-  });
- */
 let url = "./data.json";
 const myList = document.getElementById("arbetserfarenheter");
 const myListTwo = document.getElementById("utbildningar");
@@ -41,18 +24,6 @@ async function getSomething() {
       myListTwo.innerHTML += `<li>${Utbildningar.Education}</li>`;
       myListTwo.innerHTML += `<li>${Utbildningar.School}</li>`;
     });
-    /* console.log(Utbildningar); */
-
-    /* for (let i = 0; i < Year.length; i++) {
-      console.log("i: ", i);
-      let li = document.createElement("li");
-      li.innerHTML = Year[i];
-      console.log("myList.innerHTML: ", myList.innerHTML);
-      myList.innerHTML += `<li>"${Year.Arbetserfarenheter[i].Year}</li>`;
-      console.log("li: ", li);
-    } */
-    // Hämta vald data (objekt, array)
-    // Lägg till i DOM:en efter preferenser
   } else {
     console.log("HTTP-Error: " + response.status);
   }
